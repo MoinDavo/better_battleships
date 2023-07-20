d9d5acf5953a23241457301262bc4d0213743354
MoinDavo <davo.frewin@gmail.com>
2023-07-21 00:00:24 +0200

    formatting commenting

--- Field.h
+++ Field.h
@@ -1,10 +1,17 @@
 #include "Ship.h"
+/**
+ * Class to store the game fields.
+ * There are 3 Fields:
+ * 1. to store a reference to the ships (Ship)
+ * 2. to store where a player has shot (bool)
+ * 3. to store the player will see (char)
+ */
 class Field
 {
 private:
-    bool bool_field[16][16]; // stores where the player has "shot"
-    char char_field[16][16]; // stores the chars the player sees
     Ship *ship_field[16][16]; // stores the placed ships
+    bool bool_field[16][16];  // stores where the player has "shot"
+    char char_field[16][16];  // stores the chars the player sees
 
     bool check_field_empty(); // checks if the ship field is empty
 
