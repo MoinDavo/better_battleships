5841349cb7daa630d5f92da334a06473f7f9e0cb
MoinDavo <davo.frewin@gmail.com>
2023-07-21 00:02:15 +0200

    .

--- Field.h
+++ Field.h
@@ -21,10 +21,3 @@ public:
 
     void place_ship(Ship *);
 };
-
-/*
-so I'll be needing 3 of these:
-1. to store a reference to the ships (Ship)
-2. to store where a player has shot (bool)
-3. to store the player will see (char)
-*/
